7fcd37e1b882cd6e230cc8951265fe0fd199c057
yoshilarkins <yoshilarkins@gmail.com>
2025-01-20 02:46:21 -0500

    basic messaging for queueAttack

--- java/src/dbekinalkar/lib/message/EncodeDecode.java
+++ java/src/dbekinalkar/lib/message/EncodeDecode.java
@@ -1,14 +1,38 @@
 package dbekinalkar.lib.message;
 
 public class EncodeDecode {
+    
+    final static int posInfoOpCode = 0b0000;
+    final static int buildUnitsOpCode = 0b0001;
+    final static int queueAttackOpCode = 0b0004;
     /*
     Opcodes
-    0001 - Build Units
+    1 Position Opcodes
+        [opcode (4) | x (6) | y (6) | data (16)]
+        0000 - Position Info
+        0001 - Build Units
+        0010 - 
+        0011 - 
+        0100 - Queue Attack
 
      */
-    static int encodeBuild_Units(int x, int y, int unit_type, bool towerOrUnit){
-        // [opcode (4) | x addr (6) | y addr (6) | unit type (2) | tower / unit bit (1) | ]
-        int opcode = 0b0001;
+    static public int encodeQueueAttack(int x, int y, int round){
+        return encodeP(posInfoOpCode, x, y, round);
+    }
 
+    static Message decodeBytes(int bytes){
+        Message msg = new Message();
+        msg.opcode = bytes >>> 28;
+        if (msg.opcode < 0b0101){
+            msg.x = (bytes >>> 22) & 0x3F;
+            msg.y = (bytes >>> 22) & 0x3F;
+            msg.rounds = bytes & 0xFFFF;
+        }
+        return msg;
+    }
+
+    static private int encodeP(int opcode, int x, int y, int data){
+        int msg = opcode << 28 | x << 22 | y << 16 | data;
+        return msg;
     }
 }
--- java/src/dbekinalkar/lib/message/Message.java
+++ java/src/dbekinalkar/lib/message/Message.java
@@ -1,5 +1,19 @@
 package dbekinalkar.lib.message;
 
-public abstract class Message {
+public class Message {
+    static int opcode;
+    static int x;
+    static int y;
+    static int data;
+    
+    static int rounds;
+
+    public Message() {
+        opcode = 0;
+        x = 0;
+        y = 0;
+        data = 0;
+        rounds = 0;
+    }
 
 }
