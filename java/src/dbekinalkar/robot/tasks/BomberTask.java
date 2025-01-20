40f1464387a7a22f19e255a312c3b1fc227c65c2
IsaacGrossman <grossmi@purdue.edu>
2025-01-20 18:55:08 -0500

    andrew

---
+++ java/src/dbekinalkar/robot/tasks/BomberTask.java
@@ -0,0 +1,138 @@
+package dbekinalkar.robot.tasks;
+
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import dbekinalkar.lib.task_manager.Task;
+import dbekinalkar.robot.Robot;
+
+import java.util.Random;
+
+public class BomberTask extends Task {
+    // If in top part of map
+    static int spiralStep = 0;
+    static int spiralRadius = 1;
+    static int stepsTaken = 0;
+    static int amountSteps = 1;
+    static Direction[] spiralDirections = {Direction.SOUTHEAST, Direction.SOUTHWEST, Direction.NORTHWEST, Direction.NORTHEAST};
+
+    static int layerLoop = 0;
+    static int directionCount = 0;
+
+    //Andrew's stupid variables
+    static Direction[] lateralDirections = {Direction.EAST, Direction.SOUTH, Direction.WEST, Direction.SOUTH};
+    static int counter = 0;
+    static int lateralDir = 0;
+    static boolean True = true;
+    static boolean False = false;
+
+    public BomberTask(Robot r) {
+        super(r);
+    }
+
+    @Override
+    public boolean setup() {
+        return true;
+    }
+
+
+    public void run() throws GameActionException {
+        //if (this.r.rc.getMapHeight() > 20) {
+        //
+        //}
+//        if (this.r.rc.getRoundNum() > 20) {
+//            return;
+            if (!this.r.rc.isActionReady()) return;
+//            if (!this.r.rc.senseMapInfo(this.r.rc.getLocation()).getPaint().isAlly() && counter == 5) {
+//                if (rc.canPaint(rc.getLocation())) {
+//                    rc.attack(rc.getLocation());
+//                    counter = 0;
+//                    System.out.println("Painting");
+//                }
+//            }
+//            else if (counter == 5) {
+//                counter = 0;
+//                System.out.println("Resetting counter");
+//            }
+//            if (rc.senseMapInfo(rc.getLocation().add(lateralDirections[lateralDir])).getPaint().isAlly()) {
+//                lateralDir = (lateralDir + 1) % 4;
+//                counter = 0;
+//            }
+//            if (rc.canMove(lateralDirections[lateralDir])) {
+//                rc.move(lateralDirections[lateralDir]);
+//                counter += 1;
+//            }
+//        else {
+//            lateralDir = (lateralDir + 1) % 4;
+//            counter = 0;
+//            System.out.println("Setting dir to down");
+//            System.out.println(lateralDirections[lateralDir].name());
+//        }
+
+
+            // Attack only if the location is unpainted or has enemy paint
+            if (!this.r.rc.senseMapInfo(this.r.rc.getLocation()).getPaint().isAlly()) {
+                this.r.rc.attack(this.r.rc.getLocation());
+            }
+
+
+            if (this.r.rc.isMovementReady()) {
+                //Direction moveDir = [Direction.NORTH];
+                Direction moveDir = spiralDirections[spiralStep];
+                if (this.r.rc.canMove(moveDir)) {
+
+                    if (directionCount <= layerLoop) {
+                        this.r.rc.move(moveDir);
+
+                        directionCount++;
+                    }
+                    if (directionCount > layerLoop) {
+                        directionCount = 0;
+                        spiralStep++;
+                    }
+                    if (spiralStep == 4) {
+                        spiralStep = 0;
+                        layerLoop++;
+                    }
+
+                } else {
+                    Direction altDir;
+
+                    switch (moveDir){
+                            case SOUTHWEST:
+                                moveDir = Direction.NORTH;
+                                altDir = Direction.EAST;
+                                break;
+                            case NORTHWEST:
+                                moveDir = Direction.EAST;
+                                altDir = Direction.SOUTH;
+                                break;
+                            case NORTHEAST:
+                                moveDir = Direction.SOUTH;
+                                altDir = Direction.WEST;
+                                break;
+                            case SOUTHEAST:
+                                moveDir = Direction.WEST;
+                                altDir = Direction.NORTH;
+                                break;
+                            default:
+                                altDir = Direction.NORTHWEST;
+                                break;
+
+                        }
+                        if(this.r.rc.canMove(moveDir)) {
+                            this.r.rc.move(moveDir);
+                        }else{
+                            this.r.rc.move(altDir);
+                        }
+//                        if (this.r.rc.canMove(dir)) {
+//                            this.r.rc.move(dir);
+//                            //layerLoop--;
+//                            return;
+//                        }
+
+                }
+            }
+        }
+    }
+
+
