7534d2fe9a37d9213464dbda16dad535ce7e59b3
IsaacGrossman <grossmi@purdue.edu>
2025-01-20 00:35:33 -0500

    andrew

---
+++ java/src/dbekinalkar/lib/message/EncodeDecode.java
@@ -0,0 +1,14 @@
+package dbekinalkar.lib.message;
+
+public class EncodeDecode {
+    /*
+    Opcodes
+    0001 - Build Units
+
+     */
+    static int encodeBuild_Units(int x, int y, int unit_type, bool towerOrUnit){
+        // [opcode (4) | x addr (6) | y addr (6) | unit type (2) | tower / unit bit (1) | ]
+        int opcode = 0b0001;
+
+    }
+}
